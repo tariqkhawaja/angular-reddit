1814bdd6676e509bab6d3508f86797a0d000ab20
tariqkhawaja <tariqzkhawaja@comcast.net>
2023-01-07 14:19:22 -0700

    Update article.component.ts

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -17,7 +17,6 @@ export class ArticleComponent implements OnInit {
 <<<<<<< HEAD
 	this.article = new Article("","",0);
 =======
-    article = new Article("","",0);
 >>>>>>> deeb9151842ed419d46ee27ce9d74832d63c2143
 >>>>>>> 89cf07e3427ce27b5e5fcf113bb8aaadd775517b
   }
