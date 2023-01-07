02494f2f9ebe3c5f2d3694b52b68877326413fc4
Tariq Khawaja <tariqzster@gmail.com>
2023-01-07 14:30:17 -0700

    Revert "Update article.component.ts"
    This reverts commit deeb9151842ed419d46ee27ce9d74832d63c2143.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -11,15 +11,7 @@ export class ArticleComponent implements OnInit {
   @Input() article: Article;
 
   constructor() {
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	this.article = new Article("","",0);
-=======
     article = new Article("","",0);
->>>>>>> deeb9151842ed419d46ee27ce9d74832d63c2143
->>>>>>> 89cf07e3427ce27b5e5fcf113bb8aaadd775517b
   }
 
   voteUp(): boolean {
@@ -33,6 +25,6 @@ export class ArticleComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if(this.ngInit) { this.ngInit(); }
   }
-
 }
